82a887500df9ee96ca45cda811fe485d3a93db5c
jorgecc <jcastro@eftec.cl>
2022-04-20 09:57:35 -0400

    segunda version

--- class EjemploDefinicionVariables {.java
+++ class EjemploDefinicionVariables {.java
@@ -4,8 +4,9 @@ class EjemploDefinicionVariables {
         // 1) las variables se definen dentro de una funcion
         // 2) los tipos de variables
         int entero=20;
-        float decimal=30.4;
-        bool boleano=true;
+        double decimal=30.4;
+        float flotante=30.4f;
+        boolean boleano=true;
         String texto="hola mundo";
         // 3) puedo definir una variable sin asignar valor
         int entero2; // toma el valor por defecto 0 (indefinida)
