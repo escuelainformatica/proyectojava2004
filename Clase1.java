b9f2b97855d1aa5d09eb58c0844bf9af9bddbd65
jorgecc <jcastro@eftec.cl>
2022-04-20 09:53:03 -0400

    primera version

---
+++ Clase1.java
@@ -0,0 +1,39 @@
+
+public class Clase1 {
+    // campos
+    public int campo;
+
+    // funciones o metodos
+    public static void main(String args[]) {
+        // void = no regresa nada
+        // int = entero
+
+        // variable
+        int numero=20;
+        long numerolargo=30;
+
+        // float = decimal
+        float decimal=20.3;
+        bool boleano=true; // false
+        
+        // char (poco comun de usar)
+        char car='a';
+
+        // texto
+        String texto="hola";
+
+        texto="mundo";
+
+        // int,float,bool,string
+
+
+
+        // aqui no puedo ocupar numero2
+
+        System.out.println("hola mundo");
+    } // finaliza funcion main
+
+
+    // constructor
+    
+}
---
+++ class EjemploDefinicionVariables {.java
@@ -0,0 +1,17 @@
+class EjemploDefinicionVariables {
+
+    public static void main(String args[]) {
+        // 1) las variables se definen dentro de una funcion
+        // 2) los tipos de variables
+        int entero=20;
+        float decimal=30.4;
+        bool boleano=true;
+        String texto="hola mundo";
+        // 3) puedo definir una variable sin asignar valor
+        int entero2; // toma el valor por defecto 0 (indefinida)
+
+        // asigna un valor
+        texto="hello world";
+
+    }
+}
